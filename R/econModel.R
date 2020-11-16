ad68e08cae13a6ac0615cd5c327fc87d4885a1b6
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2020-11-15 18:38:27 -0600

    doc updates

--- R/econModel.R
+++ R/econModel.R
@@ -1,5 +1,51 @@
 #' econModel: Meaningful Social and Economic Data from ALFRED and elsewhere
 #'
+#' @section XTS Manipulation and Derivation:
+#' \preformatted{
+#' explode
+#'
+#' ifelse.xts
+#' is.na.xts
+#'
+#' timeEvent
+#' lagXts
+#' absChg
+#' relChg
+#' diffXts
+#' runRanks
+#' }
+#' @section St. Louis ALFRED (and FRED):
+#' \preformatted{
+#' fredAttributes
+#' estimLastUpdated
+#' vinDates
+#' getALFRED
+#' getSymbols.ALFRED
+#'
+#' toPeriod
+#' }
+#' @section Data Combinations:
+#' \preformatted{
+#' DetectFullRows
+#' DetectOnlyNonEmptyVarsInRows
+#' DetectEmptyRows
+#' RemoveEmptyRows
+#' }
+#' @section American Association of Individual Investors (AAII)  StockInvestor Pro:
+#' \preformatted{
+#' getAAIISIProDate
+#' copyAAIISIProDBFs
+#' }
+#' @section Willem Ligtenberg:
+#' \preformatted{
+#' unlockEnvironment
+#' forceAssignInNamespace
+#' }
+#' @section Miscellaneous:
+#' \preformatted{
+#' cSort
+#' }
+#'
 #' @docType package
 #' @name econModel
 "_PACKAGE"
