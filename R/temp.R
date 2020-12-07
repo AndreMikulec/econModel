9d53b3e98e4b2510d81a3d6b5338dd0d1b8af6b4
Andre Mikulec <Andre_Mikulec@Hotmail.com>
2020-12-07 01:52:35 -0600

    stLouisFEDdaily2Weekly stressIndex FSI2(started) getYieldCurve(in progress)

---
+++ R/temp.R
@@ -0,0 +1,114 @@
+
+#' Download US Treasury Yield Curve Data
+#'
+#' @description
+#' Downloads US Treasury yield curve data from the US Treasury web site.
+#'
+#' @param base String. Required. The base URL for the data service, defaulting to http://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData. If the month or year arguments are not NULL, then the function modifies this URL to parameterize the download request.
+#' @param yearThe desired year number or NULL for all years (default)
+#' @param month	 The desired month number or NULL for all months (default).
+#' @return xts object
+#' @examples
+#' \dontrun{
+#' xlist <- getYieldCurve(base = "http://data.treasury.gov/feed.svc/DailyTreasuryLongTermRateData?$filter=month(QUOTE_DATE)%20eq%2012%20and%20year(QUOTE_DATE)%20eq%202020")
+#' }
+#' @importFrom tryCatchLog tryCatchLog
+#' @importFrom curl curl_fetch_memory
+#' @importFrom XML xmlParse
+#' @importFrom XML xmlToList
+#' @importFrom DescTools DoCall
+#' @importFrom xts as.xts
+#' @export
+getYieldCurve <- function (base = "http://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData",
+                           year = NULL, month = NULL){
+tryCatchLog::tryCatchLog({
+
+  location <- base
+  yloc <- mloc <- doc <- NULL
+  yloc <- if (!is.null(year))
+    paste0("year(NEW_DATE)%20eq%20", year)
+  mloc <- if (!is.null(month))
+    paste0("month(NEW_DATE)%20eq%20", month)
+  parameters <- ""
+  if (!is.null(yloc)  && !is.null(mloc)) {
+    parameters = paste0("?$filter=", mloc, "%20and%20", yloc)
+  }
+  else {
+    if (!is.null(yloc))
+      parameters = paste("?$filter=", yloc, sep = "")
+    if (!is.null(mloc))
+      parameters = paste("?$filter=", mloc, sep = "")
+  }
+
+  req <- curl::curl_fetch_memory(paste(location, parameters, sep = ""))
+  # XML object
+  doc <- XML::xmlParse(rawToChar(req$content))
+
+  if (is.null(doc)) {
+    warning(paste("Could not parse the location.", location))
+    return(NULL)
+  }
+  message("Download and parse complete.  Converting to list...")
+  x <- XML::xmlToList(doc)
+  message("List conversion complete.  Converting to frame...")
+  updated = x[["updated"]]
+  x <- x[!Names(x) %in% c("title", "id", "updated", "link",".attrs" )]
+  y <- lapply(x, function(xx) {
+
+    yy <- data.frame(lapply(xx$content$properties, function(xxx) {
+
+      if(is.list(xxx) && length(xxx$text)) {
+        text <- xxx$text
+      } else {
+        text <- xxx
+      }
+      if(text == "N/A") {
+        text <- NA_character_
+      }
+      return(text)
+    }))
+
+    # remove
+    yy[grepl( "^.*Id$", Names(yy))] <- NULL
+
+    # 2nd entry is the *DATE (somewhere, there, always is a _DATE)
+    colnames(yy)[grepl( "^.*_DATE$", Names(yy))] <- "DATE"
+    yy[["DATE"]] <- as.POSIXct(yy[["DATE"]])
+
+    # remove (if any)
+    yy[grepl( "^.*_FACTOR$", Names(yy))] <- NULL
+
+    yy
+  })
+  # data.frame is y
+
+  message("Row bind conversion beginning.")
+  y <- DescTools::DoCall(rbind, y)
+  message("Row bind conversion complete.")
+
+  rownames(y) <- NULL
+
+  # remainder convert to real numbers
+  y <- data.frame(mapply(function(yy,colname) {
+    # save the factor column (if any)
+    browser()
+    if(!grepl("^.*_TYPE$", colname) && !colname == "DATE") {
+        as.numeric(yy)
+    } else {
+        yy
+    }
+  }, y, colnames(y), SIMPLIFY = F))
+  message("Frame conversion complete.")
+
+  if(any(grepl("^.*_TYPE$", colnames(y)))) {
+    message("xcast beginning.")
+    y <- xcast(y, IndexVar = "DATE", ValueVar = "RATE")
+    # xts object is out
+    message("xcast complete.")
+  } else {
+    # index
+    rownames(y) <- y[["DATE"]]; y[["DATE"]] <- NULL
+    y <- xts::as.xts(y)
+  }
+  y
+}, write.error.dump.folder = getOption("econModel.tryCatchLog.write.error.dump.folder"))}
